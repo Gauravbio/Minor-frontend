b0366e7545f1b7e57f6bca47d9bea1d9f3473bc6
Harshal Choudhary <111169576+harshalchoudhary11@users.noreply.github.com>
2023-01-15 13:24:58 +0530

    Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../App.css'
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h1>Login Page</h1>
+      <div className='Container'>
+        <form action='#'>
+
+          <div className='Input-class'>
+            <input type={"input"} placeholder={"User Name & Email"} className={"User"} />
+          </div>
+
+          <div className='Input-class'>
+            <input type={"password"} placeholder={"Password"} className={"User"}   />
+          </div>
+
+          <div className='button'>
+            <button type={"submit"} value={Login} className="Login">Login</button>
+           <button className='signup'><Link to={"/signup"}>Signup</Link></button>
+          </div>
+       
+        
+
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login
